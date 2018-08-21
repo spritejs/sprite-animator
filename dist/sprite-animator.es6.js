7904db6467f06af6f9a6aec45354a24ae110a8e3
akira-cn <akira.cn@gmail.com>
2018-08-21 13:43:34 +0800

    build es6 with commonjs2

--- dist/sprite-animator.es6.js
+++ dist/sprite-animator.es6.js
@@ -1,14 +1,5 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["SpriteAnimator"] = factory();
-	else
-		root["SpriteAnimator"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -4176,4 +4167,3 @@ module.exports = function (isEntries) {
 
 /***/ })
 /******/ ]);
-});
